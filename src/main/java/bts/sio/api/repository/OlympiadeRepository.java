dd12aaf8eef14c18041fdcc8d21ac8c408532612
DJAE <djaeardi80@gmail.com>
2023-09-20 11:41:54 +0200

    Creation Inter OlympiadeRepository

---
+++ src/main/java/bts/sio/api/repository/OlympiadeRepository.java
@@ -0,0 +1,7 @@
+package bts.sio.api.repository;
+
+import bts.sio.api.model.Olympiade;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OlympiadeRepository extends CrudRepository<Olympiade, Long>  {
+}
