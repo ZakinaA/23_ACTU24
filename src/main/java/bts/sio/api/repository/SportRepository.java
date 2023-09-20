117faca35b1de1651b3f465d64029a8d4e352be6
DJAE <djaeardi80@gmail.com>
2023-09-20 09:59:15 +0200

    Création classe SportRespository/SportService

---
+++ src/main/java/bts/sio/api/repository/SportRepository.java
@@ -0,0 +1,8 @@
+package bts.sio.api.repository;
+
+import bts.sio.api.model.Sport;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SportRepository extends CrudRepository<Sport, Long> {
+}
+
---
+++ src/main/java/bts/sio/api/service/SportService.java
@@ -0,0 +1,30 @@
+package bts.sio.api.service;
+
+import bts.sio.api.model.Sport;
+import bts.sio.api.repository.SportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class SportService {
+    @Autowired
+    private SportRepository sportRepository;
+
+    public Optional<Sport> getSport(final Long id) {
+        return sportRepository.findById(id);
+    }
+
+    public Iterable<Sport> getSports() {
+        return sportRepository.findAll();
+    }
+
+    public void deleteSport(final Long id) {
+        sportRepository.deleteById(id);
+    }
+
+    public Sport saveAthlete(Sport sport) {
+        Sport savedSport = sportRepository.save(sport);
+        return savedSport;
+    }
+}
+
