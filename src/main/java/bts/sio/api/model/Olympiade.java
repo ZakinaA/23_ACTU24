57a68dcd1a68ee0fd9800a695c8f97b73f26ae3e
DJAE <djaeardi80@gmail.com>
2023-09-20 11:40:23 +0200

    Creation classe Olympiade

---
+++ src/main/java/bts/sio/api/model/Olympiade.java
@@ -0,0 +1,28 @@
+package bts.sio.api.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "olympiade")
+public class Olympiade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="numero")
+    private String numero;
+
+    @Column(name="annee")
+    private String annee;
+
+    @Column(name="ville")
+    private String ville;
+
+    @ManyToOne
+    @JoinColumn(name = "pays_id")
+    private Pays pays;
+}
