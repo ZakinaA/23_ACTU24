5751e9ede23ea0aad43165629a5d55fd3190dafa
DJAE <djaeardi80@gmail.com>
2023-09-21 20:38:35 +0200

    Création de la classe SportController

---
+++ src/main/java/bts/sio/api/controller/SportController.java
@@ -0,0 +1,87 @@
+package bts.sio.api.controller;
+
+import bts.sio.api.service.SportService;
+import bts.sio.api.model.Sport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class SportController {
+
+    @Autowired
+    private SportService sportService;
+
+    /**
+     * Create - Add a new sport
+     * @param sport An object sport
+     * @return The sport object saved
+     */
+    @PostMapping("/sport")
+    public Sport createSport(@RequestBody Sport sport) {
+        return sportService.saveSport(sport);
+    }
+
+    /**
+     * Read - Get one sport
+     * @param id The id of the sport
+     * @return An sport object full filled
+     */
+    @GetMapping("/sport/{id}")
+    public Sport getSport(@PathVariable("id") final Long id) {
+        Optional<Sport> sport = sportService.getSport(id);
+        if(sport.isPresent()) {
+            return sport.get();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Read - Get all sports
+     * @return - An Iterable object of Sport full filled
+     */
+    @GetMapping("/sports")
+    public Iterable<Sport> getSports() {
+        return sportService.getSports();
+    }
+
+    /**
+     * Update - Update an existing sport
+     * @param id - The id of the sport to update
+     * @param sport - The sport object updated
+     * @return
+     */
+    @PutMapping("/sport/{id}")
+    public Sport updateSport(@PathVariable("id") final Long id, @RequestBody Sport sport) {
+        Optional<Sport> e = sportService.getSport(id);
+        if(e.isPresent()) {
+            Sport currentSport = e.get();
+
+            String nom = sport.getNom();
+            if(nom != null) {
+                currentSport.setNom(nom);
+            }
+            String descritif = sport.getDescriptif();
+            if(descritif != null) {
+                currentSport.setDescriptif(descritif);;
+            }
+
+            sportService.saveSport(currentSport);
+            return currentSport;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Delete - Delete an sport
+     * @param id - The id of the sport to delete
+     */
+    @DeleteMapping("/sport/{id}")
+    public void deleteSport(@PathVariable("id") final Long id) {
+        sportService.deleteSport(id);
+    }
+
+}
