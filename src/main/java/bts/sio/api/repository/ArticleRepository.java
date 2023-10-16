4de5fd02774315a5c60a08feb0a90b492f751ff3
JulesTurpin <ts1sio@sio301-11.sio.bts>
2023-10-16 15:45:57 +0200

    Ajout d'une méthode pour rechercher un article

--- src/main/java/bts/sio/api/repository/ArticleRepository.java
+++ src/main/java/bts/sio/api/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package bts.sio.api.repository;
 import bts.sio.api.model.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    List<Article> findByTitreContaining(String motCle);
 }
--- src/main/java/bts/sio/api/service/ArticleService.java
+++ src/main/java/bts/sio/api/service/ArticleService.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Data
@@ -28,6 +29,11 @@ public class ArticleService {
     public void deleteArticle(final Long id) {
         articleRepository.deleteById(id);
     }
+
+    public List<Article> chercherArticlesParMotCle(String motCle) {
+        return articleRepository.findByTitreContaining(motCle);
+    }
+
 
     public Article saveArticle(Article article) {
         Article savedArticle = articleRepository.save(article);
